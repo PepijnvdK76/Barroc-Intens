1264ae83d7c8e4a13618093a7211e91023789dc0
ThomasvanGastel2 <71122683+ThomasvanGastel2@users.noreply.github.com>
2022-10-11 11:10:41 +0200

    pages controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/finance', [PagesController::class, 'finance']);
+
 
 require __DIR__.'/auth.php';
