564070ea497ff02b97f47d7bdd887ff5aa2d2d96
TjKalau <71122246+TjKalau@users.noreply.github.com>
2022-10-13 15:57:00 +0200

    bkr check styling

--- resources/views/web-app/finance/edit.blade.php
+++ resources/views/web-app/finance/edit.blade.php
@@ -5,12 +5,28 @@
     <form method="POST" action="{{route('finance.update', $company)}}">
         @csrf
         @method("PUT")
-            <label class="text-white" for="">{{$company->name}}</label>
-            <label class="text-white" for="">{{$company->phone}}</label>
-            <label class="text-white" for="">{{$company->street}}</label>
-            <label class="text-white" for="">{{$company->house_number}}</label>
-            <label class="text-white" for="">{{$company->city}}</label>
-            <label class="text-white" for="">{{$company->country_code}}</label>
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col" class="text-white">Naam</th>
+                <th scope="col" class="text-white">Nummer</th>
+                <th scope="col" class="text-white">Adres</th>
+                <th scope="col" class="text-white">Huisnummer</th>
+                <th scope="col" class="text-white">Stad</th>
+                <th scope="col" class="text-white">Land</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td><label class="text-white" for="">{{$company->name}}</label></td>
+                <td><label class="text-white" for="">{{$company->phone}}</label></td>
+                <td><label class="text-white" for="">{{$company->street}}</label></td>
+                <td><label class="text-white" for="">{{$company->house_number}}</label></td>
+                <td><label class="text-white" for="">{{$company->city}}</label></td>
+                <td><label class="text-white" for="">{{$company->country_code}}</label></td>
+            </tr>
+            </tbody>
+        </table>
         <div class="form-group mt-3">
             <select class="form-control" name="bkr_checked_at">
                 <option value="{{$company->bkr_checked_at}}">{{$company->bkr_checked_at}}</option>
