c9b250a13b3763c21b935b2a8c182b3f030a9887
ThomasvanGastel2 <71122683+ThomasvanGastel2@users.noreply.github.com>
2022-10-13 15:39:13 +0200

    Update edit.blade.php

--- resources/views/web-app/finance/edit.blade.php
+++ resources/views/web-app/finance/edit.blade.php
@@ -1,18 +1,24 @@
-<form method="POST" action="{{route('finance.update', $company)}}">
-    @csrf
-    @method("PUT")
-    <label for="">{{$company->name}}</label>
-    <label for="">{{$company->phone}}</label>
-    <label for="">{{$company->street}}</label>
-    <label for="">{{$company->house_number}}</label>
-    <label for="">{{$company->city}}</label>
-    <label for="">{{$company->country_code}}</label>
-    <select name="bkr_checked_at">
-        <option value="{{$company->bkr_checked_at}}">{{$company->bkr_checked_at}}</option>
-        <option value="Niet gecheckt">Niet gecheckt</option>
-        <option value="goed">goed</option>
-        <option value="fout">fout</option>
-    </select>
+@extends('layouts.app')
 
-    <button type="submit" value="update">Update</button>
-</form>
+@section('content')
+
+    <form method="POST" action="{{route('finance.update', $company)}}">
+        @csrf
+        @method("PUT")
+            <label class="text-white" for="">{{$company->name}}</label>
+            <label class="text-white" for="">{{$company->phone}}</label>
+            <label class="text-white" for="">{{$company->street}}</label>
+            <label class="text-white" for="">{{$company->house_number}}</label>
+            <label class="text-white" for="">{{$company->city}}</label>
+            <label class="text-white" for="">{{$company->country_code}}</label>
+        <div class="form-group mt-3">
+            <select class="form-control" name="bkr_checked_at">
+                <option value="{{$company->bkr_checked_at}}">{{$company->bkr_checked_at}}</option>
+                <option value="Niet gecheckt">Niet gecheckt</option>
+                <option value="goed">goed</option>
+                <option value="fout">fout</option>
+            </select>
+        </div>
+        <button type="submit" class="btn bg-primary btn-primary mt-3" style="margin-bottom: 28%">Submit</button>
+    </form>
+@endsection
