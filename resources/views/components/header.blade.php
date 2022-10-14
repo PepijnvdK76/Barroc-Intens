69d21c433925ad0034f6b2b501d799d81c16096a
pvdk31 <pvdk.7@outlook.com>
2022-10-14 09:42:42 +0200

    Header responsive

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,35 +1,37 @@
 <header class="header">
-    <div class="navbar-area fixed-top">
-        <div class="container">
-            <div class="row align-items-center">
-                    <nav class="navbar navbar-expand-lg">
-                        <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
-                            <ul id="nav" class="navbar-nav">
-                                <li class="nav-item">
-                                    <div>
-                                        <a href="{{ route('index') }}">
-                                        <img  src="{{ Vite::asset('public/content/img/logo6_klein.png') }}" width=75%" alt="...">
-                                        </a>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="page-scroll" href="{{ route('bonen') }}">Bonen</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="page-scroll" href="#section3">Aparaten</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <a class="pr-4" href="{{ route('dashboard') }}">
-                            Dashboard
-                        </a>
-                        <a href="{{ route('offerte') }}">
-                            <div class="contactBtn p-4">
-                                <p>Contact</p>
-                            </div>
-                        </a>
-                    </nav>
-            </div>
+    <div class="d-flex align-items-center fixed-top navbar-area">
+        <div class="p-2">
+            <a href="{{ route('index') }}">
+                <img  src="{{ Vite::asset('public/content/img/logo6_klein.png') }}" width=75%" alt="...">
+            </a>
         </div>
-    </div>
+        <div class="p-2">
+            <ul id="nav" class="navbar-nav">
+                <li class="nav-item">
+                    <a class="page-scroll" href="{{ route('bonen') }}">Bonen</a>
+                </li>
+            </ul>
+        </div>
+        <div class="p-2">
+            <ul id="nav" class="navbar-nav">
+                <li class="nav-item">
+                    <a class="page-scroll" href="#section3">Aparaten</a>
+                </li>
+            </ul>
+        </div>
+        <div class="ms-auto p-2">
+            <div class="nav-item">
+                <a class="pr-4 page-scroll" href="{{ route('dashboard') }}">
+                    Dashboard
+                </a>
+            </div>
+            </div>
+                <div>
+                <a href="{{ route('offerte') }}">
+                    <div class="contactBtn">
+                        <p>Contact</p>
+                    </div>
+                </a>
+             </div>
+        </div>
 </header>
