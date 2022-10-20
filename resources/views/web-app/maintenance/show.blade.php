9224e9b07695c7e5bc3b1f277fd3d015dc687c0e
mickjacobs2004 <71122388+mickjacobs2004@users.noreply.github.com>
2022-10-20 14:54:59 +0200

    update show

--- resources/views/web-app/maintenance/show.blade.php
+++ resources/views/web-app/maintenance/show.blade.php
@@ -1,16 +1,18 @@
+
 <body>
 @extends('layouts.app')
 
 @section('content')
     <div class="row d-flex justify-content-center">
         <div class="col-lg-8 mb-4 ">
-            <h2 class="mb-3 text-white fs-3 fw-bold">Detail pagina afspraak</h2>
-            <p class="text-white fs-5"><span class="fw-bold">Bedrijfnaam: </span>{{$company->name}}</p><br>
-            <p class="text-white fs-5"><span class="fw-bold">Telefoonnummer: </span>{{$company->phone}}</p><br>
-            <p class="text-white fs-5"><span class="fw-bold">Adres: </span>{{$company->street}} {{$company->house_number}} {{$company->city}}</p><br>
-            <p class="text-white fs-5"><span class="fw-bold">Datum: </span>{{$appointment->date->format('H:i - d/m/Y')}}</p><br>
-            <p class="text-white fs-5"><span class="fw-bold">Opmerking: </span>{{$appointment->remark}}</p>
-            <a href="{{route('maintenance.index')}}" class="btn btn-primary mt-4">Ga terug naar overzicht</a>
+            <h2 class="mb-3 text-black fs-3 fw-bold">Detail pagina afspraak</h2>
+            <p class="text-black fs-5"><span class="fw-bold">Bedrijfnaam: </span>{{$company->name}}</p><br>
+            <p class="text-black fs-5"><span class="fw-bold">Telefoonnummer: </span>{{$company->phone}}</p><br>
+            <p class="text-black fs-5"><span class="fw-bold">Adres: </span>{{$company->street}} {{$company->house_number}} {{$company->city}}</p><br>
+            <p class="text-black fs-5"><span class="fw-bold">Datum: </span>{{$appointment->date->format('H:i - d/m/Y')}}</p><br>
+            <p class="text-black fs-5"><span class="fw-bold">Opmerking: </span>{{$appointment->remark}}</p>
+            <a href="{{route('maintenance.index')}}" class="btn btn-primary mt-4">Ga terug naar overzicht</a><br>
+            <a class="btn btn-primary mt-4" href="{{route('review.edit', $appointment)}}">review aanmaken</a>
         </div>
     </div>
 
@@ -41,10 +43,5 @@
     </tbody>
 </table>
 @endsection
-
-@if($appointment->done == 0)
-<a class="btn btn-warning" href="{{route('review.edit', $appointment)}}">review aanmaken</a>
-
-@endif
 </body>
 
