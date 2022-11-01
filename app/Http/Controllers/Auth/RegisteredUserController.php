c04e0315ca644f24741af3e8069937769b502e23
mickjacobs2004 <71122388+mickjacobs2004@users.noreply.github.com>
2022-11-01 11:26:02 +0100

    active redirect naar login

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -48,8 +48,6 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
-
-        return redirect(RouteServiceProvider::HOME);
+        return redirect(route('login'));
     }
 }
--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -48,7 +48,7 @@ class LoginRequest extends FormRequest
         //if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
         if (! Auth::attempt(array_merge( $this->only('email', 'password'), ['active' => 1 ]), $this->filled('remember'))) {
             RateLimiter::hit($this->throttleKey());
-            
+
 
             throw ValidationException::withMessages([
                 'email' => trans('auth.failed'),
