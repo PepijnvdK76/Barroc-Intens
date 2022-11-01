7224676bf31085d1c60f6c0fff61f61c007d65c2
mickjacobs2004 <71122388+mickjacobs2004@users.noreply.github.com>
2022-11-01 11:11:36 +0100

    active logins

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->tinyInteger('role_id')->default(0);
+            $table->boolean('active')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
