cbff6cd13c7198447332017e96a24f48c2a40cab
TjKalau <71122246+TjKalau@users.noreply.github.com>
2022-11-04 09:11:57 +0100

    opschoning van code

--- resources/views/web-app/inkoop.blade.php
+++ resources/views/web-app/inkoop.blade.php
@@ -3,50 +3,6 @@
     <h1 class="text-black">Inkoop</h1>
 @endsection
 @section('content')
-
-{{--<form action=""  class="col-9">
-    @csrf
-<div class="form-group">
-    <input type="search" name="search" id="" class="form-control" style="border-radius: 0.375rem" placeholder="Zoek een product" value="{{$search}}">
-</div>
-    <button class="btn btn-primary">Search</button>
-</form>
-<ul>
-
-        <table class="table" style="color: black">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Naam</th>
-                <th scope="col">Beschrijving</th>
-                <th scope="col">Product code</th>
-                <th scope="col">Prijs</th>
-                <th></th>
-            </tr>
-            </thead>
-            @foreach($products as $product)
-            <tbody>
-            <tr>
-                <th scope="row"><a href="{{route('inkoop.show', $product)}}">{{$product->id}}</a></th>
-                <td>{{$product->name}}</td>
-                <td>{{$product->description}}</td>
-                <td>{{$product->product_code}}</td>
-                <td>{{$product->price}}</td>
-                <td class="buttons d-flex">
-                    <a class="btn btn-warning" href="{{route('inkoop.edit' , $product)}}">aanpassen</a>
-                    <form method="POST" action="{{route('inkoop.destroy', $product)}}">
-                        @csrf
-                        @method('delete')
-                        <input class="btn btn-danger bg-danger" type="submit" value="delete">
-                    </form>
-                </td>
-            </tr>
-            </tbody>
-            @endforeach
-        </table>
-
-        <a class="btn btn-warning" href="{{route('inkoop.create')}}">Product aanmaken</a>
-</ul>--}}
 <div class="container">
     <div class="row">
         <div class="col-md-offset-1 ">
