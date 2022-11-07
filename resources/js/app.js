7bc5b97e2cf88de3f4db5c0d4e4ebafde18bf198
pvdk31 <pvdk.7@outlook.com>
2022-11-07 11:00:30 +0100

    Witte tekst header scroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,11 +11,15 @@ Alpine.start();
     ======================================= */
 window.onscroll = function () {
     const header_navbar = document.querySelector(".navbar-area");
+    const test = document.querySelector(".nav-item");
     const sticky = header_navbar.offsetTop;
+
     if (window.pageYOffset > sticky) {
-        header_navbar.classList.add("bg-transparent");
+        header_navbar.classList.add("bg-transparent","textWhite")
+        test.classList.add("bg-transparent","textWhite")
     } else {
-        header_navbar.classList.remove("bg-transparent");
+        header_navbar.classList.remove("bg-transparent","textWhite")
+        test.classList.remove("bg-transparent","textWhite")
     }
 };
 window.document.addEventListener("scroll", onScroll);
--- resources/views/web-app/company/index.blade.php
+++ resources/views/web-app/company/index.blade.php
@@ -1,35 +1,73 @@
 @extends('layouts.app')
 
 @section('content')
-    <table class="table table-striped bg-white">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">naam</th>
-            <th scope="col">telefoon</th>
-            <th scope="col">straat</th>
-            <th scope="col">huisnummer</th>
-            <th scope="col">stad</th>
-            <th scope="col">landnummer</th>
-            <th scope="col">bkr check</th>
-            <th scope="col">laast geupdate</th>
-        </tr>
-        </thead>
-
-        <tbody>
-        @foreach($companies as $company)
-            <tr>
-                <th scope="row">{{$company->id}}</th>
-                <td><a href="{{route('company.show',$company)}}">{{$company->name}}</a></td>
-                <td>{{$company->phone}}</td>
-                <td>{{$company->street}}</td>
-                <td>{{$company->house_number}}</td>
-                <td>{{$company->city}}</td>
-                <td>{{$company->country_code}}</td>
-                <td>{{$company->bkr_checked_at}}</td>
-                <td>{{$company->updated_at}}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-1 ">
+                <div class="panel">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col col-sm-3 col-xs-12">
+                                <h4 class="title">Bedrijf<span>Lijst</span></h4>
+                            </div>
+                            <div class="col-sm-9 col-xs-12 text-right">
+                                <div class="btn_group">
+                                    {{--<form action="" >
+                                        @csrf
+                                        <input type="search" name="search" id="" class="form-control" placeholder="Zoek een product" value="{{$search}}">
+                                        <button class="btn btn-default">Search</button>
+                                    </form>--}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body table-responsive">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col">Bedrijfsnaam</th>
+                                <th scope="col">telefoon</th>
+                                <th scope="col">straat</th>
+                                <th scope="col">huisnummer</th>
+                                <th scope="col">stad</th>
+                                <th scope="col">landnummer</th>
+                                <th scope="col">Betaald op</th>
+                                <th scope="col">bkr check</th>
+                                <th scope="col">laast geupdate</th>
+                            </tr>
+                            </thead>
+                            @foreach($companies as $company)
+                                <tbody>
+                                <tr class="border-bottom" >
+                                    <th scope="row"></th>
+                                    <td><a href="{{route('company.show', $company)}}">{{$company->name}}</a></td>
+                                    <td>{{$company->phone}}</td>
+                                    <td>{{$company->street}}</td>
+                                    <td>{{$company->house_number}}</td>
+                                    <td>{{$company->city}}</td>
+                                    <td>{{$company->country_code}}</td>
+                                    <td>@if(empty($company->invoices->paid_at))
+                                            Nog niet betaald
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if($company->bkr_checked_at == 'goed')
+                                            <span class="badge rounded-pill text-bg-success">Goed</span>
+                                        @elseif($company->bkr_checked_at == 'fout')
+                                            <span class="badge rounded-pill text-bg-danger">Geweigerd</span>
+                                        @else
+                                            <span class="badge rounded-pill text-bg-warning">Nog niet gecheckt</span>
+                                        @endif
+                                    </td>
+                                    <td>{{$company->updated_at}}</td>
+                                </tr>
+                                </tbody>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
