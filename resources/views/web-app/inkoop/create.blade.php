78dd69e8a867d410913cfe929649a89a390b68bc
TjKalau <71122246+TjKalau@users.noreply.github.com>
2022-11-10 15:03:10 +0100

    update styling

--- resources/views/web-app/inkoop/create.blade.php
+++ resources/views/web-app/inkoop/create.blade.php
@@ -22,46 +22,48 @@
         <textarea name="description" class="form-control" id="" cols="" rows="10"> </textarea>
     </div>
     <div class="form-group" >
-        <label for="" style="border-radius: 10px;">Foto</label>
+        <label for="">Foto</label>
         <input type="text" name="image_path" class="form-control" style="border-radius: 0.375rem">
     </div>
 
     <div class="form-group" >
-        <label for="" style="border-radius: 10px;">Product code</label>
+        <label for="">Product code</label>
         <input type="text" name="product_code" class="form-control" style="border-radius: 0.375rem">
     </div>
 
     <div class="form-group" >
-        <label for="" style="border-radius: 10px;">Prijs</label>
+        <label for="">Prijs</label>
         <input type="number" step="any" name="price" class="form-control" style="border-radius: 0.375rem">
     </div>
-        <div class="form-group" >
-            <label for="" style="border-radius: 10px;">status</label>
-            <select name="status" style="color: black; border-radius: 10px;">
-                @foreach ($products as $product)
-                    <option value="{{$product->status}}">{{$product->status}}</option>
-                @endforeach
-                <option value="Momenteel leverbaar">Momenteel leverbaar</option>
-                <option value="Uit vooraad">Uit vooraad</option>
 
+    <div class="form-group" >
+        <label for="" style="border-radius: 10px;">status</label>
+        <select class="form-control" name="status" style="color: black; border-radius: 10px;">
+            @foreach ($products as $product)
+                <option value="{{$product->status}}">{{$product->status}}</option>
+            @endforeach
+            <option value="Momenteel leverbaar">Momenteel leverbaar</option>
+            <option value="Uit vooraad">Uit vooraad</option>
+        </select>
+    </div>
 
-            </select>
-        </div>
+    <div class="form-group" >
+        <label for="" style="border-radius: 10px;">amount</label>
+        <input type="number" step="any" name="amount" class="form-control" style="border-radius: 0.375rem">
+    </div>
 
-        <div class="form-group" >
-            <label for="" style="border-radius: 10px;">amount</label>
-            <input type="number" step="any" name="amount" class="form-control" style="border-radius: 0.375rem">
-        </div>
+    <div class="form-group">
+        <label for="">Categorie</label>
+        <select class="form-control" name="products_category_id" style="color: black; border-radius: 10px;">
+            <option value="1">bonen</option>
+            <option value="2">apparaten</option>
+            <option value="3">materiaal</option>
+        </select>
+    </div>
 
-    <select name="products_category_id" style="color: black; border-radius: 10px;">
-        <option value="1">bonen</option>
-        <option value="2">apparaten</option>
-        <option value="3">materiaal</option>
-
-    </select>
 
         <div >
-            <button type="submit" value="Save Company" class="btn " style="background-color: #ffd700">Send</button>
+            <button type="submit" value="Save Company" class="btn " style="background-color: #ffd700; margin-top: 10px">Send</button>
         </div>
 </form>
         </div>
